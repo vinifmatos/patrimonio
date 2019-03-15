ba7b2a9759fd36ca5f02911e63dc4f294a11146f
vinifmatos <viniciusfreire4@gamil.com>
2019-03-15 17:57:36 -0300

    implementada validação dos campos com bootstrap para nova movimentação

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,11 @@ module ApplicationHelper
     I18n.t "views.active.#{active}"
   end
 
-  def datepicker_field(form, field)
+  def datepicker_field(form, field, options = {})
     content_tag(:div, class: 'input-group date-input-group') do
       form.text_field(field,
                       value: (I18n.l(eval("form.object.#{field}")) if eval("form.object.#{field}.present?")),
-                      class: 'form-control datepicker') +
+                      class: "form-control datepicker #{options[:class]}", required: (options[:required] || false)) +
         content_tag(:div, class: 'input-group-append') do
           button_tag(fa_icon('calendar'),
                      id: 'btn-datepicker',
