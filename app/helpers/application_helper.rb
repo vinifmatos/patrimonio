dcaf20e16dcb3c185fbe6eff1a31157739beefc2
vinifmatos <viniciusfreire4@gmail.com>
2019-03-15 20:20:33 -0300

    implementado mensagens de feedback para as validaçoes do bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,17 @@ module ApplicationHelper
     I18n.t "views.active.#{active}"
   end
 
-  def datepicker_field(form, field, options = {})
+  def datepicker_field(form, field, options = {}, &block)
     content_tag(:div, class: 'input-group date-input-group') do
-      form.text_field(field,
-                      value: (I18n.l(eval("form.object.#{field}")) if eval("form.object.#{field}.present?")),
-                      class: "form-control datepicker #{options[:class]}", required: (options[:required] || false)) +
-        content_tag(:div, class: 'input-group-append') do
-          button_tag(fa_icon('calendar'),
-                     id: 'btn-datepicker',
-                     class: 'btn btn-outline-secondary', type: 'button')
-        end
+      content = form.text_field(field,
+                                value: (I18n.l(eval("form.object.#{field}")) if eval("form.object.#{field}.present?")),
+                                class: "form-control datepicker-input #{options[:class]}", required: (options[:required] || false)) +
+                content_tag(:div, class: 'input-group-append') do
+                  button_tag(fa_icon('calendar'),
+                             id: 'btn-datepicker',
+                             class: 'btn btn-outline-secondary', type: 'button')
+                end
+      content + capture(&block) if block_given?
     end
   end
 end
