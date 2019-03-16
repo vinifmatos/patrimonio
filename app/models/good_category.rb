39f397ee4dfab405d9915b3222609da214ad8412
vinifmatos <viniciusfreire4@gmail.com>
2019-03-16 20:04:17 -0300

    implementado teste de validaçoes para good_category

--- app/models/good_category.rb
+++ app/models/good_category.rb
@@ -3,4 +3,14 @@
 class GoodCategory < ApplicationRecord
   belongs_to :good_sub_kind
   has_many :goods
+
+  validates_presence_of :description
+
+  before_create :set_active
+
+  private
+
+  def set_active
+    self.active ||= true
+  end
 end
--- spec/models/good_category_spec.rb
+++ spec/models/good_category_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe GoodCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'is valid' do
+    it 'with valid attributes' do
+      good_category = build(:good_category)
+
+      expect(good_category).to be_valid
+    end
+  end
+
+  context 'is invalid' do
+    it 'without description' do
+      good_category = build(:good_category, description: nil)
+
+      expect(good_category).to_not be_valid
+    end
+
+    it 'without sub kind' do
+      good_category = build(:good_category, good_sub_kind: nil)
+
+      expect(good_category).to_not be_valid
+    end
+  end
 end
