87d60a50dd0bcbabe635edb0eb8f79947e13586c
vinifmatos <viniciusfreire4@gmail.com>
2019-03-16 20:02:58 -0300

    implementado teste de validaçoes para good_sub_kind

--- app/models/good_sub_kind.rb
+++ app/models/good_sub_kind.rb
@@ -3,4 +3,14 @@
 class GoodSubKind < ApplicationRecord
   belongs_to :good_kind
   has_many :good_categories
+
+  validates_presence_of :description
+
+  before_create :set_active
+
+  private
+
+  def set_active
+    self.active ||= true
+  end
 end
--- spec/models/good_sub_kind_spec.rb
+++ spec/models/good_sub_kind_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe GoodSubKind, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'is valid' do
+    it 'with valid attributes' do
+      good_sub_kind = build(:good_sub_kind)
+
+      expect(good_sub_kind).to be_valid
+    end
+  end
+
+  context 'is invalid' do
+    it 'without description' do
+      good_sub_kind = build(:good_sub_kind, description: nil)
+
+      expect(good_sub_kind).to_not be_valid
+    end
+
+    it 'without kind' do
+      good_sub_kind = build(:good_sub_kind, good_kind: nil)
+
+      expect(good_sub_kind).to_not be_valid
+    end
+  end
 end
