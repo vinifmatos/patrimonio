ea76ee18ea6fc7f53a011657652c6bd950940e9c
vinifmatos <viniciusfreire4@gamil.com>
2019-03-20 17:44:15 -0300

    adicionado campo de sub tipo na view da depreciação

--- app/controllers/depreciation_controller.rb
+++ app/controllers/depreciation_controller.rb
@@ -4,5 +4,10 @@ class DepreciationController < ApplicationController
   def index
     @goods = Good.active
     # @depreciations = FinancialMovement.depreciation.where(good_id: @goods.ids)
+    @kinds = (GoodKind.all.select(:id, :description).map { |k| [k.description, k.id] }).sort
+  end
+
+  def get_sub_kinds
+    @sub_kinds = GoodSubKind.where(good_kind_id: params[:id])
   end
 end
