4d366a53073e467c71eff1099869207b0dd3d049
vinifmatos <viniciusfreire4@gamil.com>
2019-03-22 17:07:15 -0300

    implementado filtro de bens na tela de depreciação, corrigido falha que fazia os selects de sub tipo e categoria ficaram com opções não filtradas

--- app/controllers/depreciation_controller.rb
+++ app/controllers/depreciation_controller.rb
@@ -3,11 +3,13 @@
 class DepreciationController < ApplicationController
   def index
     if params[:kind].blank?
-      @goods = Good.active.order(:code)
+      @goods = params[:goods].blank? ? Good.active.order(:code) : Good.active.where(id: params[:goods].split(',')).order(:code)
     else
       sub_kinds = params[:sub_kind].blank? ? GoodSubKind.where(good_kind_id: params[:kind]).pluck(:id) : params[:sub_kind]
       categories = params[:category].blank? ? GoodCategory.where(good_sub_kind_id: sub_kinds).pluck(:id) : params[:category]
-      @goods = Good.active.where(good_category_id: (params[:category].blank? ? categories : params[:category])).order(:code)
+      good_params = { good_category_id: (params[:category].blank? ? categories : params[:category]) }
+      good_params[:id] = params[:goods].split(',') unless params[:goods].blank?
+      @goods = Good.active.where(good_params).order(:code)
     end
 
     @kinds = (GoodKind.all.select(:id, :description).map { |k| [k.description, k.id] }).sort
